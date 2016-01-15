592cb6d5abcded6ae9898b59492d9b79fec59f04
Jackie Ng <jumpinjackie@gmail.com>
2016-01-16 00:26:52 +1100

    build.cmd: Fix version and errorlevel checking

--- build.cmd
+++ build.cmd
@@ -1,16 +1,19 @@
 @echo off
 SET MG_CONFIG=Release
-IF %MG_VERSION%=="3.0" SET MG_CONFIG=Release_VC11
-IF %MG_VERSION%=="2.6" SET MG_CONFIG=Release_VC11
+IF "%MG_VERSION%"=="3.0" SET MG_CONFIG=Release_VC11
+IF "%MG_VERSION%"=="2.6" SET MG_CONFIG=Release_VC11
+echo Using configuration [%MG_CONFIG%]
 pushd src\Bindings
 msbuild /p:Configuration=%MG_CONFIG%;Platform=x86 Bindings.sln
-if %errorlevel%=="1" goto error
+if errorlevel 1 goto error
 msbuild /p:Configuration=%MG_CONFIG%;Platform=x64 Bindings.sln
-if %errorlevel%=="1" goto error
+if errorlevel 1 goto error
 popd
 pushd src\Bindings\DotNet\MapGuideDotNetApi
+call dnu restore
+if errorlevel 1 goto error
 call dnu pack --configuration Release
-if %errorlevel%=="1" goto error
+if errorlevel 1 goto error
 popd
 goto end
 :error
