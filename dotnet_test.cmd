99ed57c9b10997311c283eb5666120c896cdff94
Jackie Ng <jumpinjackie@gmail.com>
2017-06-17 09:54:30 +1000

    Also run the TestRunner after building

--- dotnet_test.cmd
+++ dotnet_test.cmd
@@ -1,7 +1,11 @@
+SET MG_INSTALL_DIR=D:\mg-trunk\MgDev\Release64
 pushd src\Test\Dnx
 dotnet add src\TestCommon package MapGuideDotNetApi
 dotnet add src\TestMapGuideApi package MapGuideDotNetApi
 dotnet add src\TestRunner package MapGuideDotNetApi
 dotnet restore
-dotnet build
+dotnet build --configuration Release
+popd
+pushd src\Test\Dnx\src\TestRunner
+dotnet run -f netcoreapp1.1 "%MG_INSTALL_DIR%\Web\www\webconfig.ini" "%MG_INSTALL_DIR%\CS-Map\Dictionaries"
 popd
