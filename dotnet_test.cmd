34fd890ccb06386732529e03a77b975441f7a08e
Jackie Ng <jumpinjackie@gmail.com>
2018-05-26 21:48:55 +1000

    Update dotnet test runner script

--- dotnet_test.cmd
+++ dotnet_test.cmd
@@ -1,11 +1,14 @@
-SET MG_INSTALL_DIR=D:\mg-trunk\MgDev\Release64
-pushd src\Test\Dnx
+SET MG_INSTALL_DIR=%1
+IF "%MG_INSTALL_DIR%" == "" (
+    SET MG_INSTALL_DIR=C:\Program Files\OSGeo\MapGuide
+)
+pushd src\Test\DotNet
 dotnet add src\TestCommon package MapGuideDotNetApi
 dotnet add src\TestMapGuideApi package MapGuideDotNetApi
 dotnet add src\TestRunner package MapGuideDotNetApi
 dotnet restore
 dotnet build --configuration Release
 popd
-pushd src\Test\Dnx\src\TestRunner
-dotnet run -f netcoreapp1.1 "%MG_INSTALL_DIR%\Web\www\webconfig.ini" "%MG_INSTALL_DIR%\CS-Map\Dictionaries"
+pushd src\Test\DotNet\src\TestRunner
+dotnet run -f netcoreapp2.0 "%MG_INSTALL_DIR%\Web\www\webconfig.ini" "%MG_INSTALL_DIR%\CS-Map\Dictionaries"
 popd
