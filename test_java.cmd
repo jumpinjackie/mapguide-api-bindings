18acfdd7d16bc20a5ca4234306a751ed98b0d5a6
Jackie Ng <jumpinjackie@gmail.com>
2018-06-17 13:05:18 +1000

    #25: Fix up paths for test scripts. This will break the non-CMake build, we'll deal with that later.

--- test_java.cmd
+++ test_java.cmd
@@ -1,9 +1,11 @@
 @echo off
 set BINPREFIX=Bin64
 set CONFIG=Release64
+set RELPATH=Release\x64
 if "%1" == "Release" (
     set CONFIG=%1
     set BINPREFIX=Bin
+    set RELPATH=Release\x86
 )
 SET MG_INSTALL_DIR=%2
 IF "%MG_INSTALL_DIR%" == "" (
@@ -15,8 +17,8 @@ SET MG_JAVA_JAR_DIR=%CD%\src\Test\Java\lib
 if not exist "%MG_JAVA_BIN_DIR%" mkdir "%MG_JAVA_BIN_DIR%"
 if not exist "%MG_JAVA_JAR_DIR%" mkdir "%MG_JAVA_JAR_DIR%"
 copy /y "%CD%\sdk\%MG_VERSION%\%BINPREFIX%\*.dll" "%MG_JAVA_BIN_DIR%"
-copy /y "%CD%\packages\Java\%CONFIG%\*.dll" "%MG_JAVA_BIN_DIR%"
-copy /y "%CD%\packages\Java\%CONFIG%\*.jar" "%MG_JAVA_JAR_DIR%"
+copy /y "%CD%\packages\Java\%RELPATH%\*.dll" "%MG_JAVA_BIN_DIR%"
+copy /y "%CD%\packages\Java\%RELPATH%\*.jar" "%MG_JAVA_JAR_DIR%"
 pushd "%CD%\src\Test\Java"
 call "%ANT_HOME%\bin\ant" checkwin_external -Dmapguide.dictpath="%MG_INSTALL_DIR%\CS-Map\Dictionaries" -Djarsrc.web="%MG_JAVA_JAR_DIR%" -Dbinsrc.web="%MG_JAVA_BIN_DIR%" -Dmapguide.config.src="%MG_INSTALL_DIR%\Web\www\webconfig.ini" -Dmapguide.res.src="%MG_INSTALL_DIR%\Web\www\mapagent\Resources\mapguide_en.res"
 if %ERRORLEVEL% neq 0 echo [test]: Java test runner had one or more test failures. Check log files for more information
--- test_php.cmd
+++ test_php.cmd
@@ -1,9 +1,11 @@
 @echo off
 set BINPREFIX=Bin64
 set CONFIG=Release64
+set RELPATH=Release\x64
 if "%1" == "Release" (
     set CONFIG=%1
     set BINPREFIX=Bin
+    set RELPATH=Release\x86
 )
 SET MG_INSTALL_DIR=%2
 IF "%MG_INSTALL_DIR%" == "" (
@@ -13,8 +15,8 @@ echo Using configuration: %CONFIG%
 set PHP_ENV=%CD%\runtimes\php\%CONFIG%
 set PHP_EXT_DIR=%PHP_ENV%\ext
 copy /Y "%CD%\sdk\%MG_VERSION%\%BINPREFIX%\*.dll" "%PHP_ENV%"
-copy /Y "%CD%\packages\php\%CONFIG%\php_MapGuideApi.*" "%PHP_EXT_DIR%"
-copy /Y "%CD%\packages\php\%CONFIG%\*.php" "%CD%\src\Test\Php"
+copy /Y "%CD%\packages\php\%RELPATH%\php_MapGuideApi.*" "%PHP_EXT_DIR%"
+copy /Y "%CD%\packages\php\%RELPATH%\*.php" "%CD%\src\Test\Php"
 pushd %CD%\src\Test\Php
 set PHP_ARGS=-n -d display_errors=On -d extension_dir="%PHP_EXT_DIR%" -d extension=php_mbstring.dll -d extension=php_curl.dll -d extension=php_pdo_sqlite.dll -d extension=php_MapGuideApi.dll
 %PHP_ENV%\php.exe %PHP_ARGS% RunTests.php -config "%MG_INSTALL_DIR%\Web\www\webconfig.ini"
