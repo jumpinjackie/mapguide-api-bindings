a3e26ea4d2044d656d1fad16023eb9f8e087c716
Jackie Ng <jumpinjackie@gmail.com>
2018-06-26 02:18:16 +1000

    Update ubuntu provisioning script

--- linux_build/DotNet/Ubuntu14_x64/dotnet.sh
+++ linux_build/DotNet/Ubuntu14_x64/dotnet.sh
@@ -9,22 +9,33 @@ MG_ARCH=amd64
 SRC_AREA=~/mapguide_build
 CMAKE_BUILD_AREA=~/mapguide_build_cmake
 
-sudo apt-get update && sudo apt-get install -y build-essential git wget cmake dos2unix p7zip-full libpcre3-dev
+# Need xerces so we have its headers, which is needed by string out typemaps for .net. Buildpack does not include xerces headers because
+# they have been "configure"'d for Win32
+sudo apt-get update && sudo apt-get install -y zip build-essential git wget cmake dos2unix p7zip-full libpcre3-dev libxerces-c-dev
 git clone --depth 1 https://github.com/jumpinjackie/mapguide-api-bindings $SRC_AREA
 
 # Install .net Core SDK
 wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-2.1
-# We don't need to download the full set of MapGuide packages, this should be sufficient
-for pkg in platformbase common webextensions
-do
-    wget -q https://download.osgeo.org/mapguide/releases/$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV/Final/ubuntu14_x64/mapguideopensource-${pkg}_$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV-${MG_VER_BUILD}_${MG_ARCH}.deb
-    sudo dpkg -i mapguideopensource-${pkg}_$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV-${MG_VER_BUILD}_${MG_ARCH}.deb
-done
+wget -q https://download.osgeo.org/mapguide/releases/$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV/Final/ubuntu14_x64/mginstallubuntu.sh
+chmod +x mginstallubuntu.sh
+sudo ./mginstallubuntu.sh --headless --with-sdf --with-shp --with-sqlite
+if test "$?" -ne 0; then
+    exit 1
+fi
 cd $SRC_AREA
-./envsetupsdk.sh --with-dotnet --version $MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV.$MG_VER_BUILD
+./build_tools.sh
+./envsetupsdk.sh --with-dotnet --with-java --version $MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV.$MG_VER_BUILD
+if test "$?" -ne 0; then
+    exit 1
+fi
 mkdir -p $CMAKE_BUILD_AREA
-cd $CMAKE_BUILD_AREA
-cmake -DCMAKE_BUILD_TYPE=Release -DMG_VERSION_MAJOR=$MG_VER_MAJOR -DMG_VERSION_MINOR=$MG_VER_MINOR -DMG_VERSION_PATCH=$MG_VER_REV -DMG_CPU=64 -DWITH_JAVA=1 -DWITH_PHP=0 -DWITH_DOTNET=1 $SRC_AREA
-make
+./cmake_build.sh --with-dotnet --with-java --version $MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV.$MG_VER_BUILD --cpu 64 --working-dir $CMAKE_BUILD_AREA
+if test "$?" -ne 0; then
+    exit 1
+fi
+echo "Copying java glue library"
+cp $SRC_AREA/packages/Java/Release/x64/Ubuntu14.04.x86_64/libMapGuideJavaApi.so /mapguide_sources/packages/Java/Release/x64/Ubuntu14.04.x86_64
+echo "Copying .net glue library"
+cp $SRC_AREA/src/Bindings/DotNet/MapGuideDotNetApi/runtimes/ubuntu.14.04-x64/libMapGuideDotNetUnmanagedApi.so /mapguide_sources/src/Bindings/DotNet/MapGuideDotNetApi/runtimes/ubuntu.14.04-x64
