9670472c11082a0275007f1dc5472da8ff6d89e2
Jackie Ng <jumpinjackie@gmail.com>
2018-06-26 21:11:02 +1000

    #32: Add top-level box builder script. Adjust base box provisioning scripts to cater for the environment needed to build the MapGuide API bindings (not MapGuide itself)

--- linux_build/packer/scripts/mapguide/ubuntu32.sh
+++ linux_build/packer/scripts/mapguide/ubuntu32.sh
@@ -5,11 +5,11 @@ sudo dpkg-reconfigure --frontend=noninteractive dash
 # Guard against (http://askubuntu.com/questions/41605/trouble-downloading-updates-due-to-a-hash-sum-mismatch-error)
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get update
-sudo apt-get install -y build-essential bison automake libtool libpng-dev libjpeg62-dev libxslt-dev libfreetype6-dev libpcre3-dev openjdk-7-jdk subversion lintian ant scons dos2unix libldap-dev libssl-dev libcurl4-openssl-dev libexpat-dev libmysqlclient-dev unixODBC-dev python-dev libpq-dev libcppunit-dev
-# For producing .tar.xz archives
-sudo apt-get install -y xz-utils
-# For deb packaging
-sudo apt-get install -y rsync
+sudo apt-get install -y zip build-essential git wget cmake dos2unix p7zip-full libpcre3-dev
 # To support Java test suite
 sudo apt-get install -y ant-contrib
+# Buildpack deps. Need xerces so we have its headers, which is needed by string out typemaps for .net. Buildpack does not include xerces headers because
+# they have been "configure"'d for Win32
+sudo apt-get install -y libxerces-c-dev
+# Register JAVA_HOME
 echo "JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-i386" | sudo tee --append /etc/environment
--- linux_build/packer/scripts/mapguide/ubuntu64.sh
+++ linux_build/packer/scripts/mapguide/ubuntu64.sh
@@ -5,13 +5,15 @@ sudo dpkg-reconfigure --frontend=noninteractive dash
 # Guard against (http://askubuntu.com/questions/41605/trouble-downloading-updates-due-to-a-hash-sum-mismatch-error)
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get update
-sudo apt-get install -y build-essential bison automake libtool libpng-dev libjpeg62-dev libxslt-dev libfreetype6-dev libpcre3-dev openjdk-7-jdk subversion lintian ant scons dos2unix libldap-dev libssl-dev libcurl4-openssl-dev libexpat-dev libmysqlclient-dev unixODBC-dev python-dev libpq-dev libcppunit-dev
-# For producing .tar.xz archives
-sudo apt-get install -y xz-utils
-# For deb packaging
-sudo apt-get install -y rsync
-# For 32-bit swig binary in MapGuide source
-sudo apt-get install -y g++-multilib
+sudo apt-get install -y zip build-essential git wget cmake dos2unix p7zip-full libpcre3-dev
 # To support Java test suite
 sudo apt-get install -y ant-contrib
+# Buildpack deps. Need xerces so we have its headers, which is needed by string out typemaps for .net. Buildpack does not include xerces headers because
+# they have been "configure"'d for Win32
+sudo apt-get install -y libxerces-c-dev
+# Install .net Core SDK
+wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-2.1
+# Register JAVA_HOME
 echo "JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64" | sudo tee --append /etc/environment
