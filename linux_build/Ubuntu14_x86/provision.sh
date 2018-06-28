8fb3f71e22be0299e4c87443868e32116a8aed68
Jackie Ng <jumpinjackie@gmail.com>
2018-06-28 22:55:51 +1000

    #32: Add vagrantfile for 32-bit Ubuntu

---
+++ linux_build/Ubuntu14_x86/provision.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+MG_VER_MAJOR=$1
+MG_VER_MINOR=$2
+MG_VER_REV=$3
+MG_VER_BUILD=$4
+MG_ARCH=amd64
+
+SRC_AREA=~/mapguide_build
+CMAKE_BUILD_AREA=~/mapguide_build_cmake
+
+git clone --depth 1 https://github.com/jumpinjackie/mapguide-api-bindings $SRC_AREA
+
+echo "Going to download: http://192.168.0.2/releases/$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV/Final/ubuntu14_x86/mginstallubuntu.sh"
+wget -q http://192.168.0.2/releases/$MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV/Final/ubuntu14_x86/mginstallubuntu.sh
+chmod +x mginstallubuntu.sh
+sudo ./mginstallubuntu.sh --headless --with-sdf --with-shp --with-sqlite
+if test "$?" -ne 0; then
+    exit 1
+fi
+cd $SRC_AREA
+./build_tools.sh
+./envsetupsdk.sh --with-java --version $MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV.$MG_VER_BUILD
+if test "$?" -ne 0; then
+    exit 1
+fi
+mkdir -p $CMAKE_BUILD_AREA
+./cmake_build.sh --with-java --version $MG_VER_MAJOR.$MG_VER_MINOR.$MG_VER_REV.$MG_VER_BUILD --cpu 64 --working-dir $CMAKE_BUILD_AREA
+if test "$?" -ne 0; then
+    exit 1
+fi
+./test_dotnet.sh
+if test "$?" -ne 0; then
+    exit 1
+fi
+./test_java.sh
+if test "$?" -ne 0; then
+    exit 1
+fi
+echo "Copying java glue library"
+cp $SRC_AREA/packages/Java/Release/x86/Ubuntu14.04.i686/libMapGuideJavaApi.so /mapguide_sources/packages/Java/Release/x86/Ubuntu14.04.i686
