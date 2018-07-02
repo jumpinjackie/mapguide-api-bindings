a47bd45424dac0293bf3a49e3a4e59d1810a9443
Jackie Ng <jumpinjackie@gmail.com>
2018-07-03 00:39:35 +1000

    Rework full framework .net test script to work with the revised project layout

--- test_dotnet_full.cmd
+++ test_dotnet_full.cmd
@@ -13,28 +13,23 @@ SET MG_INSTALL_DIR=%1
 IF "%MG_INSTALL_DIR%" == "" (
     SET MG_INSTALL_DIR=C:\Program Files\OSGeo\MapGuide
 )
-SET MG_NUGET_PKG_DIR=%CD%\packages_fullfx
-pushd src\Test\DotNet\src\TestCommonFull
-nuget install MapGuideDotNetApi -o %MG_NUGET_PKG_DIR% -Version %MG_PKG_VER%
-if errorlevel neq 0 goto error_popd
-popd
-pushd src\Test\DotNet\src\TestMapGuideApiFull
-nuget install MapGuideDotNetApi -o %MG_NUGET_PKG_DIR% -Version %MG_PKG_VER%
-if errorlevel neq 0 goto error_popd
-popd
-pushd src\Test\DotNet\src\TestRunnerFull
-nuget install MapGuideDotNetApi -o %MG_NUGET_PKG_DIR% -Version %MG_PKG_VER%
-if errorlevel neq 0 goto error_popd
-popd
 pushd src\Test\DotNet
+dotnet add src\TestCommonFull package MapGuideDotNetApi --version %MG_PKG_VER%
+if %errorlevel% neq 0 goto error_popd
+dotnet add src\TestMapGuideApiFull package MapGuideDotNetApi --version %MG_PKG_VER%
+if %errorlevel% neq 0 goto error_popd
+dotnet add src\TestRunnerFull package MapGuideDotNetApi --version %MG_PKG_VER%
+if %errorlevel% neq 0 goto error_popd
+dotnet add src\TestMiscFull package MapGuideDotNetApi --version %MG_PKG_VER%
+if %errorlevel% neq 0 goto error_popd
 nuget restore FullFramework.sln
-if errorlevel neq 0 goto error_popd
+if %errorlevel% neq 0 goto error_popd
 msbuild /p:Configuration=Release FullFramework.sln
-if errorlevel neq 0 goto error_popd
+if %errorlevel% neq 0 goto error_popd
 popd
-pushd src\Test\DotNet\src\TestRunnerFull\bin\x64\Release
+pushd src\Test\DotNet\src\TestRunnerFull\bin\x64\Release\net461
 TestRunnerFull --web-config-path "%MG_INSTALL_DIR%\Web\www\webconfig.ini" --dictionary-path "%MG_INSTALL_DIR%\CS-Map\Dictionaries" --test-data-root %THIS_DIR%\src\TestData
-if errorlevel neq 0 goto error_popd
+if %errorlevel% neq 0 goto error_popd
 popd
 goto done
 :error_popd
